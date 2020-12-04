12931d51076c8d32bf1a2681bf22633a89f9bc82
[jahedul29] <[jahedulh1@gmail.com]>
2020-12-04 15:09:40 +0600

    changing cors origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://simplechatappjahed.netlify.app",
     methods: ["GET", "POST"],
   },
 });
